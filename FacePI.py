c87c2ac144e3f119e8187f0ef35a811159643259
unforgattableeternalproject <ptyc4076@gmail.com>
2021-12-13 11:54:49 +0800

    FacePI

--- FacePI.py
+++ FacePI.py
@@ -26,7 +26,40 @@ class FacePI:
         with open(configpath, 'r', encoding='utf-8') as f:
             config = json.load(f)
         return config
-        
+    def detectLocalImage(self, imagepath):
+        headers = {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': self.readConfig()['api_key'],
+        }
+
+        params = urllib.parse.urlencode({
+            'returnFaceId': 'true',
+            'returnFaceLandmarks': 'false',
+            'returnFaceAttributes': 'age, gender',
+
+            'returnRecognitionModel': 'false',
+            'detectionModel': 'detection_01',
+            'faceIdTimeToLive': '86400',
+        })
+
+        print('imagepath=', imagepath)
+        requestbody = open(imagepath, "rb").read()
+
+        try:
+            conn = http.client.HTTPSConnection(self.readConfig()['host'])
+            conn.request("POST", "/face/v1.0/detect?%s" % params. requestbody, headers)
+            response = conn.getresponse()
+            data = response.read()
+            json_face_detect = json.loads(str(data, 'UTF-8'))
+            print("detectLocalImage.faces=", json_face_detect)
+
+            conn.close()
+
+            print("detectLocalImage:", f"{imagepath} detected {len(json_face_detect)} people.")
+
+            return json_face_detect
+        except Exception as e:
+            print("[Errno {0}]Connection Failed {1}".format(e.errno, e.strerror))
 
     def detectImageUrl(self, imageurl):
         headers = {
@@ -59,7 +92,7 @@ class FacePI:
             return json_face_detect
 
         except Exception as e:
-            print("[Error {0}]Connection Failed {1}".format(e.errno, e.strerror))
+            print("[Errno {0}]Connection Failed {1}".format(e.errno, e.strerror))
 
         def Signin(self):
             imageurl = 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTE4MDAzNDEwNzg5ODI4MTEw/barack-obama-12782369-1-402.jpg'

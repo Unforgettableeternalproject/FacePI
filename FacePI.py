5f8f6f1cdcd0a6179b681558ae4d7a396a7ffe2b
Bernie <95599456+Unforgettableeternalproject@users.noreply.github.com>
2021-12-20 10:28:33 +0800

    Submit Change

--- FacePI.py
+++ FacePI.py
@@ -9,9 +9,20 @@ configpath = os.path.join(basepath, 'Config.json')
 
 class FacePI:
     
-        def Signin(self):
-            imageurl = 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTE4MDAzNDEwNzg5ODI4MTEw/barack-obama-12782369-1-402.jpg'
-            IncludedClasses.ClassFacePI.Face().detectImageUrl(imageurl)
+    def __init__(self) -> None:
+        self.detectIU = IncludedClasses.ClassFacePI.Face().detectImageUrl()
+        self.detectLI = IncludedClasses.ClassFacePI.Face().detectLocalImage()
+
+    def Signin(self):
+        imageurl = 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTE4MDAzNDEwNzg5ODI4MTEw/barack-obama-12782369-1-402.jpg'
+#        imagepath = r'C:/Users/user/Desktop/Bernie/FacePII/barack-obama-12782369-1-402.jpg'
+
+        self.detectIU(imageurl)
+ #       self.detectLI(imagepath)
+
+pi = FacePI()
+
+pi.Signin()
 
 if __name__ == '__main__':
     fire.Fire(FacePI)
